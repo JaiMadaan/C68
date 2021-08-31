4ad7f3dfde87f6f1fe854bf1670f66a11f45b523
JaiMadaan <77677308+JaiMadaan@users.noreply.github.com>
2021-08-31 17:15:22 +0530

    Add files via upload

---
+++ screens/fbScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from 'react-native';
+
+export default class fbScreen extends React.Component{
+    render(){
+        return(
+            <View styles={{}}>
+<Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold',padding:100,marginTop:200 }}>Facebook </Text>
+            </View>
+        )
+    }
+}
---
+++ screens/instaScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class instaScreen extends React.Component{
+    render(){
+        return(
+            <View styles={{}}>
+<Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold',padding:100,marginTop:200 }}>Instagram </Text>
+            </View>
+        )
+    }
+}
